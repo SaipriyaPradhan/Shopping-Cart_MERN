a3825118ca6934a1eccefce689c9a331b2d7ea7c
SaipriyaPradhan <71316648+SaipriyaPradhan@users.noreply.github.com>
2023-09-24 20:42:39 +0530

    Add files via upload

---
+++ build/precache-manifest.f1265f84e3562f88579f122e6d372f83.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "8d9b2beb8eaa5a4b5068",
+    "url": "/static/css/main.dce6f42f.chunk.css"
+  },
+  {
+    "revision": "8d9b2beb8eaa5a4b5068",
+    "url": "/static/js/main.3d652e5d.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "cab835a2c546434e9d7f",
+    "url": "/static/css/2.067a53ae.chunk.css"
+  },
+  {
+    "revision": "cab835a2c546434e9d7f",
+    "url": "/static/js/2.6ed90945.chunk.js"
+  },
+  {
+    "revision": "613f22c9efcad0fb2088a6c3b561ee9e",
+    "url": "/index.html"
+  }
+];
